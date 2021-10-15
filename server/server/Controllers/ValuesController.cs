6ad6cfa98597f95994be55138bda5f8838c828a7
Yael <yaelikip@gmail.com>
2021-10-15 10:49:47 +0300

    init commit

---
+++ server/server/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("GetCity/{city}")]
+        public List<string> GetCity(string s)
+        {
+            List<string> list = new List<string>();
+            list.Add("hello");
+            return list;
+        }
+    }
+}
